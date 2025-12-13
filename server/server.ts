be7c72cf355f9d6bfcd3266a1e32b3d5008e6085
zeyadsleem <zeyadsleem1@gmail.com>
2025-12-13 02:50:00 +0200

    use pm2

--- server/server.ts
+++ server/server.ts
@@ -19,6 +19,7 @@ import { requestLoggerMiddleware } from './middleware/loggerMiddleware';
   app.use(requestLoggerMiddleware);
 
   // Public endpoints
+  app.get('/healthz', (req, res) => res.send({ status: 'OK ✌️' }));
   app.post('/v1/signup', signUpHandler);
   app.post('/v1/signin', signInHandler);
 
@@ -30,5 +31,5 @@ import { requestLoggerMiddleware } from './middleware/loggerMiddleware';
 
   app.use(errHandler);
 
-  app.listen(3000);
+  app.listen(process.env.PORT || 3000);
 })();
